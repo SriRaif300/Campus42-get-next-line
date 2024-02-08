5b137540586c88b168fe0bdf3c7aab519e81061e
Camilo <garatejo00@gmail.com>
2024-02-08 19:12:52 +0100

    add files inicials

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgaratej <cgaratej@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 19:03:21 by cgaratej          #+#    #+#             */
+/*   Updated: 2024/02/08 19:10:58 by cgaratej         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
